814bed296e07ba02b1b3202cf4f3e982c435e60b
Guy Kahana <guy16@meet.mit.edu>
2014-12-04 20:01:25 +0200

    input exercise

--- computer.py
+++
@@ -1,3 +0,0 @@
-print ("lingo")
-print ("nomad")
-print ("pilot")
---
+++ mayamylove.py
@@ -0,0 +1,8 @@
+x=input()
+if (int(x)==7):
+	print ("cool! this is my lucky number")
+elif (x=="2"):
+	print("2 is ok too")
+else:
+	print ("wrong answer!")
+
