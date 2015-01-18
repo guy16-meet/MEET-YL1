4ac6ba240b9a4f4f78b8ee5886381284e3cbe7aa
guy16-meet <guy16@meet.mit.edu>
2015-01-18 11:44:11 +0200

    Update paintprogram.py

--- paintprogram.py
+++ paintprogram.py
@@ -13,76 +13,65 @@ print("ENJOY!")
 
 def size_1():    
     turtle.shapesize(1,1,1)
-    
-turtle.getscreen().onkeypress(size_1,"1")
-turtle.getscreen().listen()
+    turtle.getscreen().onkeypress(size_1,"1")
+    turtle.getscreen().listen()
 
 def size_2():    
     turtle.shapesize(2,2,2)
-    
-turtle.getscreen().onkeypress(size_2,"2")
-turtle.getscreen().listen()
+    turtle.getscreen().onkeypress(size_2,"2")
+    turtle.getscreen().listen()
 
 
 def size_4():    
     turtle.shapesize(4,4,4)
+    turtle.getscreen().onkeypress(size_4,"4")
+    turtle.getscreen().listen()
     
-turtle.getscreen().onkeypress(size_4,"4")
-turtle.getscreen().listen()
 def size_3():    
     turtle.shapesize(3,3,3)
-    
-turtle.getscreen().onkeypress(size_3,"3")
-turtle.getscreen().listen()
+    turtle.getscreen().onkeypress(size_3,"3")
+    turtle.getscreen().listen()
 
 def turtle_shape():    
     turtle.shape("turtle")
-    
-turtle.getscreen().onkeypress(turtle_shape,"Right")
-turtle.getscreen().listen()
+    turtle.getscreen().onkeypress(turtle_shape,"Right")
+    turtle.getscreen().listen()
 
 def triangle_shape():    
     turtle.shape("triangle")
-    
-turtle.getscreen().onkeypress(triangle_shape,"Left")
-turtle.getscreen().listen()
+    turtle.getscreen().onkeypress(triangle_shape,"Left")
+    turtle.getscreen().listen()
 
 def circle_shape():    
     turtle.shape("circle")
-    
-turtle.getscreen().onkeypress(circle_shape,"Up")
-turtle.getscreen().listen()
+    turtle.getscreen().onkeypress(circle_shape,"Up")
+    turtle.getscreen().listen()
 
 def square_shape():    
     turtle.shape("square")
-    
-turtle.getscreen().onkeypress(square_shape,"Down")
-turtle.getscreen().listen()
+    turtle.getscreen().onkeypress(square_shape,"Down")
+    turtle.getscreen().listen()
 
 
 def blue_color():    
     turtle.color("blue")
-    
-turtle.getscreen().onkeypress(blue_color,"b")
-turtle.getscreen().listen()
+    turtle.getscreen().onkeypress(blue_color,"b")
+    turtle.getscreen().listen()
 
 def red_color():    
     turtle.color("red")
-    
-turtle.getscreen().onkeypress(red_color,"r")
-turtle.getscreen().listen()
+    turtle.getscreen().onkeypress(red_color,"r")
+    turtle.getscreen().listen()
 
 def green_color():    
     turtle.color("green")
-    
-turtle.getscreen().onkeypress(green_color,"g")
-turtle.getscreen().listen()
+    turtle.getscreen().onkeypress(green_color,"g")
+    turtle.getscreen().listen()
 
 def yellow_color():    
     turtle.color("yellow")
-    
-turtle.getscreen().onkeypress(yellow_color,"y")
-turtle.getscreen().listen()
+    turtle.getscreen().onkeypress(yellow_color,"y")
+    turtle.getscreen().listen()
 
 turtle.color("blue")
 turtle.shape("circle")
